554d051f9189d818b89e7e6b3e607fd411aea680
ccagle8 <ccagle8@f02fa33f-f54b-7b49-610b-75779d30549c>
2011-01-23 15:21:54 +0000

    initial load

---
+++ trunk/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" > <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Temp Site</title>
+  <meta name="description" content="" >
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="index, follow">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"> 
+	
+	<link href="assets/css/reset.css" rel="stylesheet">
+	<link href="assets/css/style.css?v=0.1" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<link rel="stylesheet" type="text/css" href="assets/css/ie.css" media="screen">
+	<![endif]--> 
+	
+	<!--[if lt IE 7 ]>
+    <script src="assets/js/dd_belatedpng.js"></script>
+    <script> DD_belatedPNG.fix('img, .png_bg'); //fix any <img> or .png_bg background-images </script>
+  <![endif]-->
+ 
+ 	<!-- Base Javascript -->
+ 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js"></script>
+	<script type="text/javascript" src="assets/js/custom.js?v=0.1"></script>
+	
+</head> 
+<body <?php page_id(); ?> >
+	
+	<div class="wrapper">
+		<header>
+
+				<nav></nav>
+			
+    </header>
---
+++ trunk/index.php
@@ -0,0 +1,15 @@
+<?php
+include('includes/functions.php');
+
+
+
+?>
+<?php get_template('header', 'Title of homepage'); ?>
+
+<article>
+	<h1>Hello World</h1>
+	
+</article>
+
+
+<?php get_template('footer'); ?>
